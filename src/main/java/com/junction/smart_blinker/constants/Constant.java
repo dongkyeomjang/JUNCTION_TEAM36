837f9b47a0e4ca9c349678fcec4f2d0e6d6a33df
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-08-11 10:14:12 +0900

    feat: project init setting

---
+++ src/main/java/com/junction/smart_blinker/constants/Constant.java
@@ -0,0 +1,13 @@
+package com.junction.smart_blinker.constants;
+
+public class Constant {
+    public static Integer RED = 0;
+    public static Integer REDYELLOW = 1;
+    public static Integer LEFTTRUNRED = 2;
+    public static Integer YELLOW = 3;
+    public static Integer GREEN = 4;
+    public static Integer AUTOMODE = 0;
+    public static Integer MANUALMODE = 1;
+    public static Integer THREECOLOR = 0;
+    public static Integer FOURCOLOR = 1;
+}
---
+++ src/test/java/com/junction/smart_blinker/SmartBlinkerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.junction.smart_blinker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SmartBlinkerApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
