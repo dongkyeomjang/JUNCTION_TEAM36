b8891f0374538e7d2e290bfe539b01568a0681f4
장동겸 <jangelliot0404@dgu.ac.kr>
2024-08-11 10:15:19 +0900

    ✨feat: project init setting (#4)

---
+++ src/main/java/com/junction/smart_blinker/config/SchedulerConfig.java
@@ -0,0 +1,19 @@
+package com.junction.smart_blinker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+public class SchedulerConfig {
+
+    @Bean
+    public ThreadPoolTaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(10);
+        scheduler.setThreadNamePrefix("TrafficScheduler-");
+        scheduler.initialize();
+        return scheduler;
+    }
+}
+
